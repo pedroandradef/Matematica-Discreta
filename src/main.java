d3e476bb6300b59e56e1c157448a3d08cd66527c
Pedro Paulo Andrade <pedroandradef@outlook.com>
2019-05-27 17:34:09 -0300

    hashmap

--- src/main.java
+++ src/main.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class main {
     public static void main(String[] args) {
-        ArrayList<String> teste1 = new ArrayList<String>();
-        ArrayList<String> teste2 = new ArrayList<String>();
-        ArrayList<String> intersecao = new ArrayList<String>();
-        HashSet<String> uniao = new HashSet<String>();
 
-        teste1.add("a");
+        Map<String,HashSet<String>> testeMap = new HashMap<String,HashSet<String>>();
+        HashSet<String> teste1 = new HashSet<String>();
+        HashSet<String> teste2 = new HashSet<String>();
+
+
+
         teste1.add("8");
         teste1.add("&");
         teste1.add("-5");
@@ -17,22 +20,27 @@ public class main {
         teste2.add("&");
         teste2.add("8");
         teste2.add("-5");
-        teste2.add("A");
+        teste2.add("A"
+        teste2.add("a");
 
 
-        teste1.forEach(s -> criaIntercessao(s, teste2, intersecao));
+        //teste1.forEach(s -> criaIntercessao(s, teste2, intersecao));
 
-        criaUniao(teste1,teste2,uniao);
+        testeMap.put("A",teste1);
+        System.out.println(testeMap.get("A"));
 
+        testeMap.put("B",teste2);
 
+        testeMap.put("C",criaUniao(testeMap.get("A"), testeMap.get("B")));
 
-        for(String s : uniao)
-            System.out.println(s);
+        System.out.println(testeMap.get("C"));
 
     }
 
 
     public static void criaIntercessao(String s, ArrayList<String> array, ArrayList<String> intercessao) {
+
+
         for (int i = 0; i < array.size(); i++) {
             if (s.equals(array.get(i))) {
                 intercessao.add(s);
@@ -40,11 +48,15 @@ public class main {
         }
     }
 
-    public static void criaUniao(ArrayList<String> conjunto1, ArrayList<String> conjunto2, HashSet<String> uniao) {
-        for(int i = 0; i < conjunto1.size(); i++)
-            uniao.add(conjunto1.get(i));
+    public static HashSet criaUniao(HashSet<String> conjunto1, HashSet<String> conjunto2) {
+        HashSet<String> uniao = new HashSet<String>();
 
-        for(int i = 0; i < conjunto2.size(); i++)
-            uniao.add(conjunto2.get(i));
+        for(String s : conjunto1)
+            uniao.add(s);
+
+        for(String s : conjunto2)
+            uniao.add(s);
+
+        return uniao;
     }
 }
